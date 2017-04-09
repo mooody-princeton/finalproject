8e7593cbdb16cb0af9ccb45912ff9aa1d85f8378
Xiao Yang Yu <Flamberge@nat-oitwireless-inside-vapornet100-c-129.Princeton.EDU>
2017-04-09 00:39:04 -0400

    PRAISE THE SUN mood tracking complete

--- mooody/public/javascripts/angularApp.js
+++ mooody/public/javascripts/angularApp.js
@@ -225,6 +225,17 @@ app.factory('posts', ['$http', 'auth', function($http, auth) {
     return o;
 }]);
 
+// Services ***********************************************
+
+// Load social mood info upon accessing website, for sidebar
+app.service('socialinfo', function($http) {
+    var promise = $http.get('/socialmood').success(function(data) {
+        var socialinfo = data;
+        return socialinfo;
+    });
+    return promise;
+});
+
 // Controllers ********************************************
 
 // Main Controller (home page)
@@ -339,74 +350,67 @@ app.controller('NavCtrl', ['$scope', 'auth',
     }]);
 
 // Sidebar Controller
-app.controller('SidebarCtrl', ['$scope', 'auth',
-    function($scope, auth) {
+app.controller('SidebarCtrl', ['$scope', 'auth', 'socialinfo',
+    function($scope, auth, socialinfo) {
+        // Wait until we get the social mood info...
+        socialinfo.then(function(data) {
+            auth.socialmood = data.data;
+            $scope.currentSocialMood = auth.socialmood;
+        
+            // Create chart
+            var chart = new Chartist.Pie('.ct-chart', {
+              series: [auth.socialmood[0].happy, auth.socialmood[0].sad, auth.socialmood[0].angry],
+              labels: ["happy", "angry", "sad"]
+            }, {
+              donut: true,
+              showLabel: false
+            });
+
+            // Draw chart
+            chart.on('draw', function(data) {
+              if(data.type === 'slice') {
+                // Get the total path length in order to use for dash array animation
+                var pathLength = data.element._node.getTotalLength();
+                // Set a dasharray that matches the path length as prerequisite to animate dashoffset
+                data.element.attr({
+                  'stroke-dasharray': pathLength + 'px ' + pathLength + 'px'
+                });
+                // Create animation definition while also assigning an ID to the animation for later sync usage
+                var animationDefinition = {
+                  'stroke-dashoffset': {
+                    id: 'anim' + data.index,
+                    dur: 1000,
+                    from: -pathLength + 'px',
+                    to:  '0px',
+                    easing: Chartist.Svg.Easing.easeOutQuint,
+                    // We need to use `fill: 'freeze'` otherwise our animation will fall back to initial (not visible)
+                    fill: 'freeze'
+                  }
+                };
+                // If this was not the first slice, we need to time the animation so that it uses the end sync event of the previous animation
+                if(data.index !== 0) {
+                  animationDefinition['stroke-dashoffset'].begin = 'anim' + (data.index - 1) + '.end';
+                }
+                // We need to set an initial value before the animation starts as we are not in guided mode which would do that for us
+                data.element.attr({
+                  'stroke-dashoffset': -pathLength + 'px'
+                });
+                // We can't use guided mode as the animations need to rely on setting begin manually
+                // See http://gionkunz.github.io/chartist-js/api-documentation.html#chartistsvg-function-animate
+                data.element.animate(animationDefinition, false);
+              }
+            });
+
+            // Dynamically update chart every few seconds with new data
+            window.setInterval(function() {chart.update({series:[auth.socialmood[0].happy, auth.socialmood[0].sad, auth.socialmood[0].angry]})}, 10000);
+        });
+
+        // The following doesn't rely on the socialinfo service's promise
         $scope.isLoggedIn = auth.isLoggedIn;
         $scope.currentUser = auth.currentUser;
         $scope.currentUserId = auth.currentUserId;
         $scope.currentMood = auth.usermood;
-        $scope.currentSocialMood = auth.socialmood;
-
-        // chart data
-        var chart = new Chartist.Pie('.ct-chart', {
-          series: [3, 2, 1],
-          labels: ["happy", "angry", "sad"]
-        }, {
-          donut: true,
-          showLabel: false
-        });
-
-        chart.on('draw', function(data) {
-          if(data.type === 'slice') {
-            // Get the total path length in order to use for dash array animation
-            var pathLength = data.element._node.getTotalLength();
-
-            // Set a dasharray that matches the path length as prerequisite to animate dashoffset
-            data.element.attr({
-              'stroke-dasharray': pathLength + 'px ' + pathLength + 'px'
-            });
-
-            // Create animation definition while also assigning an ID to the animation for later sync usage
-            var animationDefinition = {
-              'stroke-dashoffset': {
-                id: 'anim' + data.index,
-                dur: 1000,
-                from: -pathLength + 'px',
-                to:  '0px',
-                easing: Chartist.Svg.Easing.easeOutQuint,
-                // We need to use `fill: 'freeze'` otherwise our animation will fall back to initial (not visible)
-                fill: 'freeze'
-              }
-            };
-
-            // If this was not the first slice, we need to time the animation so that it uses the end sync event of the previous animation
-            if(data.index !== 0) {
-              animationDefinition['stroke-dashoffset'].begin = 'anim' + (data.index - 1) + '.end';
-            }
-
-            // We need to set an initial value before the animation starts as we are not in guided mode which would do that for us
-            data.element.attr({
-              'stroke-dashoffset': -pathLength + 'px'
-            });
-
-            // We can't use guided mode as the animations need to rely on setting begin manually
-            // See http://gionkunz.github.io/chartist-js/api-documentation.html#chartistsvg-function-animate
-            data.element.animate(animationDefinition, false);
-          }
-        });
-
-        // For the sake of the example we update the chart every time it's created with a delay of 8 seconds
-        chart.on('created', function() {
-          if(window.__anim21278907124) {
-            clearTimeout(window.__anim21278907124);
-            window.__anim21278907124 = null;
-          }
-          window.__anim21278907124 = setTimeout(chart.update.bind(chart), 10000);
-        });
-
-        // END CHART DATA
-
-
+        
         // Update user mood
         $scope.setMoodTo = function(moodString) {
             // If same mood as current mood, don't do anything
