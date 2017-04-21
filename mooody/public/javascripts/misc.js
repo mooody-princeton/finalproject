e3ded05346ba4964bf5b50d6c2696877dec52487
Katelyn Neese <kneese@princeton.edu>
2017-04-21 14:18:19 -0400

    animated scrolling

--- mooody/public/javascripts/misc.js
+++ mooody/public/javascripts/misc.js
@@ -14,6 +14,9 @@ function scrollFunction() {
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     console.log("here");
-    document.body.scrollTop = 0; // For Chrome, Safari and Opera
-    document.documentElement.scrollTop = 0; // For IE and Firefox
+    //document.body.scrollTop= 0; // For Chrome, Safari and Opera
+    //document.documentElement.scrollTop = 0; // For IE and Firefox
+     $('html, body').animate({scrollTop:0}, 'slow');
+
+
 };
