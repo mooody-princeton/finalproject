9b26cb82ddf0e4e504087fded429f71a914d46ca
Xiao Yang Yu <xyyxiaoyangyu@gmail.com>
2017-04-28 23:07:42 -0400

    Fix

--- mooody/models/SocialMood.js
+++ mooody/models/SocialMood.js
@@ -3,10 +3,14 @@
 var mongoose = require('mongoose');
 
 var SocialMoodSchema = new mongoose.Schema({
-  happy: {type: Number, default: 0},
-  sad: {type: Number, default: 0},
-  angry: {type: Number, default: 0}
+  happy: {type: Number, default: 1},
+  relaxed: {type: Number, default: 1},
+  couldbebetter: {type: Number, default: 1},
+  stressed: {type: Number, default: 1},
+  sad: {type: Number, default: 1},
+  angry: {type: Number, default: 1}
 });
+// Initialize everything to 1 so the chart is never empty (that'd be ugly!)
 
 // Decrement social mood count
 SocialMoodSchema.methods.decrement = function(mood, cb) {
